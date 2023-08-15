17dd504d802536a45011047c7ca61a7365b4d858
Stephen <Stephen@rconfig.com>
2023-08-15 21:11:54 +0100

    V6Core Dockerfiles WIP

---
+++ .dockerfiles/dockerkillall.sh
@@ -0,0 +1,11 @@
+# docker-compose down
+docker rm -f $(docker ps -a -q)
+docker volume rm $(docker volume ls -q)
+rm -fr mysql/*
+rm -fr src/rconfig
+
+docker-compose up -d -build
+docker ps -a
+#docker rmi $(docker images -a -q)
+#docker rmi $(docker images --filter dangling=true -q)
+#docker system df
