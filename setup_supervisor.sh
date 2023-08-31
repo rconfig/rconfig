5d8dc7b6a31c75a74022bb1c477d7417473c1167
Stephen <stephen@rconfig.com>
2023-08-31 10:23:07 -0400

    setup_supervisor.sh crlf

--- setup_supervisor.sh
+++ setup_supervisor.sh
@@ -5,8 +5,8 @@ if [ -f /etc/redhat-release ]; then  SUPDIR=/etc/supervisord.d; fi;
 if [ -f /etc/lsb-release ]; then  SUPDIR=/etc/supervisor/conf.d; fi;
 if [ -f $SUPDIR/horizon_supervisor.ini ]; then unlink $SUPDIR/horizon_supervisor.ini; fi
 sed -i -e s+PWD+$PWD+g $PWD/horizon_supervisor.ini
-if [ -f /etc/redhat-release ]; then  ln -s $PWD/horizon_supervisor.ini $SUPDIR/horizon_supervisor.ini fi;
-if [ -f /etc/lsb-release ]; then  ln -s $PWD/horizon_supervisor.ini $SUPDIR/horizon_supervisor.conf fi;
+if [ -f /etc/redhat-release ]; then  ln -s $PWD/horizon_supervisor.ini $SUPDIR/horizon_supervisor.ini; fi;
+if [ -f /etc/lsb-release ]; then  ln -s $PWD/horizon_supervisor.ini $SUPDIR/horizon_supervisor.conf; fi;
 if [ -f /etc/redhat-release ]; then systemctl restart supervisord; fi;
 if [ -f /etc/lsb-release ]; then sudo systemctl restart supervisor; fi;
 supervisorctl status
