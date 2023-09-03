8328ee3a8d5e42125d3d0e57e6ce1fa2ff02994a
Stephen <stephen@rconfig.com>
2023-09-03 07:28:35 -0400

    Choose a template

--- resources/js/forms/TemplatesForm.vue
+++ resources/js/forms/TemplatesForm.vue
@@ -13,29 +13,19 @@
                         </label>
                     </div>
                     <div class="pf-c-form__group-control">
-                        <input
-                            class="pf-c-form-control"
-                            required
-                            type="text"
-                            id="fileName"
-                            name="fileName"
-                            v-model="model.fileName"
-                            :aria-invalid="errors.fileName ? true : false"
-                            autocomplete="off"
-                        />
-                        <p v-if="errors.fileName" class="pf-c-form__helper-text pf-m-error" id="form-help-text-address-helper" aria-live="polite">
+                        <input class="pf-c-form-control" required type="text" id="fileName" name="fileName"
+                            v-model="model.fileName" :aria-invalid="errors.fileName ? true : false" autocomplete="off" />
+                        <p v-if="errors.fileName" class="pf-c-form__helper-text pf-m-error"
+                            id="form-help-text-address-helper" aria-live="polite">
                             {{ errors.fileName[0] }}
                         </p>
-                        <p v-if="showSelectTemplateFields" class="pf-c-form__helper-text">You may overwrite this filename if a template was selected</p>
+                        <p v-if="showSelectTemplateFields" class="pf-c-form__helper-text">You may overwrite this filename if
+                            a template was selected</p>
                     </div>
                 </div>
-                <button
-                    class="pf-c-button pf-m-link pf-u-pl-xs pf-u-pl-xs pf-u-mt-lg pf-u-mb-md"
-                    type="button"
-                    @click="showSelectTemplateFields = true"
-                    v-if="hasVendorTemplateOptions && !showSelectTemplateFields"
-                >
-                    Create from Template
+                <button class="pf-c-button pf-m-link pf-u-pl-xs pf-u-pl-xs pf-u-mt-lg pf-u-mb-md" type="button"
+                    @click="showSelectTemplateFields = true" v-if="hasVendorTemplateOptions && !showSelectTemplateFields">
+                    Click to choose from imported templates
                 </button>
                 <div class="pf-c-form__group pf-u-mt-lg" v-if="showSelectTemplateFields">
                     <div class="pf-c-form__group-label">
@@ -46,9 +36,11 @@
 
                     <div class="pf-c-select pf-m-expanded" ref="clickOutsidetarget1">
                         <span hidden>Choose an option</span>
-                        <button class="pf-c-select__toggle" type="button" @click.prevent="showVendorTemplateOptions = !showVendorTemplateOptions">
+                        <button class="pf-c-select__toggle" type="button"
+                            @click.prevent="showVendorTemplateOptions = !showVendorTemplateOptions">
                             <div class="pf-c-select__toggle-wrapper">
-                                <span class="pf-c-select__toggle-text" v-text="vendorTemplateOptionSelected.length != 0 ? vendorTemplateOptionSelected : 'Choose a vendor'"></span>
+                                <span class="pf-c-select__toggle-text"
+                                    v-text="vendorTemplateOptionSelected.length != 0 ? vendorTemplateOptionSelected : 'Choose a vendor'"></span>
                             </div>
                             <span class="pf-c-select__toggle-arrow">
                                 <i class="fas fa-caret-down" aria-hidden="true"></i>
@@ -57,9 +49,11 @@
                         <div v-if="showVendorTemplateOptions ? 'hidden' : ''">
                             <ul class="pf-c-select__menu multi-select-dropdown-overflow" role="listbox">
                                 <li role="presentation" v-for="option in vendorTemplateOptions.data" :key="option.name">
-                                    <button class="pf-c-select__menu-item" role="option" @click.prevent="getTemplatesList(option)">
+                                    <button class="pf-c-select__menu-item" role="option"
+                                        @click.prevent="getTemplatesList(option)">
                                         {{ option.name }}
-                                        <span class="pf-c-select__menu-item-icon" v-if="option.name === vendorTemplateOptionSelected">
+                                        <span class="pf-c-select__menu-item-icon"
+                                            v-if="option.name === vendorTemplateOptionSelected">
                                             <i class="fas fa-check" aria-hidden="true"></i>
                                         </span>
                                     </button>
@@ -69,7 +63,8 @@
                     </div>
                     <p class="pf-c-form__helper-text">Select a vendor first, then select a template below</p>
                     <p class="pf-c-form__helper-text pf-u-mb-xl" v-if="hasReadmeFile">
-                        <a :href="'https://github.com/rconfig/rConfig-templates/tree/master/' + vendorTemplateOptionSelected" target="_blank">View readme documents</a>&nbsp;&nbsp;
+                        <a :href="'https://github.com/rconfig/rConfig-templates/tree/master/' + vendorTemplateOptionSelected"
+                            target="_blank">View readme documents</a>&nbsp;&nbsp;
                         <i class="fas fa-external-link-alt pf-u-font-size-xs pf-u-color-400"></i>
                     </p>
                 </div>
@@ -81,9 +76,11 @@
                     </div>
                     <div class="pf-c-select pf-m-expanded" ref="clickOutsidetarget2">
                         <span hidden>Choose an option</span>
-                        <button class="pf-c-select__toggle" type="button" @click.prevent="showFileOptions = !showFileOptions">
+                        <button class="pf-c-select__toggle" type="button"
+                            @click.prevent="showFileOptions = !showFileOptions">
                             <div class="pf-c-select__toggle-wrapper">
-                                <span class="pf-c-select__toggle-text" v-text="fileOptionSelected.length != 0 ? fileOptionSelected : 'Choose a vendor'"></span>
+                                <span class="pf-c-select__toggle-text"
+                                    v-text="fileOptionSelected.length != 0 ? fileOptionSelected : 'Choose a template'"></span>
                             </div>
                             <span class="pf-c-select__toggle-arrow">
                                 <i class="fas fa-caret-down" aria-hidden="true"></i>
@@ -92,7 +89,8 @@
                         <div v-if="showFileOptions ? 'hidden' : ''">
                             <ul class="pf-c-select__menu multi-select-dropdown-overflow" role="listbox">
                                 <li role="presentation" v-for="option in listedFiles.data" :key="option.name">
-                                    <button class="pf-c-select__menu-item" role="option" @click.prevent="getTemplateFileContents(option)">
+                                    <button class="pf-c-select__menu-item" role="option"
+                                        @click.prevent="getTemplateFileContents(option)">
                                         {{ option.name }}
                                         <span class="pf-c-select__menu-item-icon" v-if="option.name === fileOptionSelected">
                                             <i class="fas fa-check" aria-hidden="true"></i>
@@ -102,7 +100,8 @@
                             </ul>
                         </div>
                     </div>
-                    <p class="pf-c-form__helper-text pf-u-warning-color-100">Warning: Selecting a template will instantly overwrite any edits in the yaml file</p>
+                    <p class="pf-c-form__helper-text pf-u-warning-color-100">Warning: Selecting a template will instantly
+                        overwrite any edits in the yaml file</p>
                 </div>
                 <div class="pf-c-form__group pf-m-action">
                     <div class="pf-c-form__group-control">
@@ -115,7 +114,8 @@
             </div>
             <div class="pf-c-form__group pf-l-grid__item pf-m-9-col">
                 <!-- by default, `copied` will be reset in 1.5s -->
-                <div class="pf-c-tooltip pf-m-top-left" role="tooltip" v-if="copied" style="z-index: 999; position: absolute; top: 26%">
+                <div class="pf-c-tooltip pf-m-top-left" role="tooltip" v-if="copied"
+                    style="z-index: 999; position: absolute; top: 26%">
                     <div class="pf-c-tooltip__arrow"></div>
                     <div class="pf-c-tooltip__content" id="tooltip-top-content">Copied!</div>
                 </div>
@@ -123,53 +123,38 @@
                     <div class="pf-c-code-editor">
                         <div class="pf-c-code-editor__header">
                             <div class="pf-c-code-editor__controls">
-                                <button class="pf-c-button pf-m-control" type="button" alt="Copy to clipboard" title="Copy to clipboard" @click="copy">
+                                <button class="pf-c-button pf-m-control" type="button" alt="Copy to clipboard"
+                                    title="Copy to clipboard" @click="copy">
                                     <i class="fas fa-copy" aria-hidden="true"></i>
                                 </button>
 
-                                <button class="pf-c-button pf-m-control" type="button" title="Download code" alt="Download code" @click="download(model.fileName)">
+                                <button class="pf-c-button pf-m-control" type="button" title="Download code"
+                                    alt="Download code" @click="download(model.fileName)">
                                     <i class="fas fa-download"></i>
                                 </button>
-                                <button class="pf-c-button pf-m-control" type="button" title="full screen" alt="full screen" @click="showConfigFullScreen">
+                                <button class="pf-c-button pf-m-control" type="button" title="full screen" alt="full screen"
+                                    @click="showConfigFullScreen">
                                     <i class="fas fa-expand"></i>
                                 </button>
 
                                 <div class="pf-c-check" style="align-content: center">
-                                    <input
-                                        class="pf-c-check__input"
-                                        type="checkbox"
-                                        id="darkmode"
-                                        name="darkmode"
-                                        @change="toggleEditorDarkMode($event)"
-                                        :checked="darkmode == 'vs-dark'"
-                                        style="margin-left: 0.5rem"
-                                    />
+                                    <input class="pf-c-check__input" type="checkbox" id="darkmode" name="darkmode"
+                                        @change="toggleEditorDarkMode($event)" :checked="darkmode == 'vs-dark'"
+                                        style="margin-left: 0.5rem" />
 
                                     <label class="pf-c-check__label" style="cursor: default">Dark Mode</label>
                                 </div>
                                 <div class="pf-c-check" style="align-content: center">
-                                    <input
-                                        class="pf-c-check__input"
-                                        type="checkbox"
-                                        id="lineNumbers"
-                                        name="lineNumbers"
-                                        @change="toggleEditorLineNumbers($event)"
-                                        :checked="lineNumbers == 'on'"
-                                        style="margin-left: 0.5rem"
-                                    />
+                                    <input class="pf-c-check__input" type="checkbox" id="lineNumbers" name="lineNumbers"
+                                        @change="toggleEditorLineNumbers($event)" :checked="lineNumbers == 'on'"
+                                        style="margin-left: 0.5rem" />
 
                                     <label class="pf-c-check__label" style="cursor: default">Line Numbers</label>
                                 </div>
                                 <div class="pf-c-check" style="align-content: center">
-                                    <input
-                                        class="pf-c-check__input"
-                                        type="checkbox"
-                                        id="lineNumbers"
-                                        name="lineNumbers"
-                                        @change="toggleEditorMinimap($event)"
-                                        :checked="minimap.enabled == 'true'"
-                                        style="margin-left: 0.5rem"
-                                    />
+                                    <input class="pf-c-check__input" type="checkbox" id="lineNumbers" name="lineNumbers"
+                                        @change="toggleEditorMinimap($event)" :checked="minimap.enabled == 'true'"
+                                        style="margin-left: 0.5rem" />
                                     <label class="pf-c-check__label" style="cursor: default">Minimap</label>
                                 </div>
                             </div>
@@ -182,10 +167,11 @@
                         </div>
                         <div class="pf-c-code-editor__main" id="pf-c-code-editor__main">
                             <code class="pf-c-code-editor__code">
-                                <div class="pf-c-code-editor__code-pre" id="pf-c-code-editor__code-pre" style="height: calc(100vh - 400px)"></div>
-                            </code>
+                                        <div class="pf-c-code-editor__code-pre" id="pf-c-code-editor__code-pre" style="height: calc(100vh - 400px)"></div>
+                                    </code>
                         </div>
-                        <p v-if="errors.code" class="pf-c-form__helper-text pf-m-error" id="form-help-text-address-helper" aria-live="polite">
+                        <p v-if="errors.code" class="pf-c-form__helper-text pf-m-error" id="form-help-text-address-helper"
+                            aria-live="polite">
                             {{ errors.code[0] }}
                         </p>
                     </div>
