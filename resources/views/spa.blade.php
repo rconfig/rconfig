a8d3c8c41431c2acfac5ad4ca747ba839d66aa27
Stephen Stack <stephen@rconfig.com>
2024-10-10 15:53:48 +0100

    Refactor layout structure in spa.blade.php for improved readability and organization

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -7,36 +7,31 @@
 
 <body contenteditable="false">
     <div id="app" tabindex="-1" id="ws-router" style="outline: none;">
-        <div>
-            <div class="pf-u-h-100vh">
-                <div class="pf-c-page">
-                    <div class="min-h-screen bg-gray-100 dark:bg-rcgray-800">
+        <div class="min-h-screen bg-gray-100 dark:bg-rcgray-800">
 
-                        <resizable-panel-group id="nav-group-1" direction="horizontal" class="min-h-[100vh] max-w-[100vw] rounded-lg border dark:bg-rcgray-900" auto-save-id="any-id" :default-size="['10%', '90%']">
+            <resizable-panel-group id="nav-group-1" direction="horizontal" class="max-w-[100vw] rounded-lg border dark:bg-rcgray-900" auto-save-id="any-id" :default-size="['10%', '90%']">
 
-                            <navigation-side></navigation-side>{{-- first resizable panel inside nav-side component --}}
+                <navigation-side></navigation-side>{{-- first resizable panel inside nav-side component --}}
 
-                            <resizable-handle id="nav-handle-1" class="bg-gray-200 dark:bg-rcgray-600" with-handle>
-                                <Icon icon="radix-icons-drag-handle-dots-2" />
-                            </resizable-handle>
+                <resizable-handle id="nav-handle-1" class="bg-gray-200 dark:bg-rcgray-600" with-handle>
+                    <Icon icon="radix-icons-drag-handle-dots-2" />
+                </resizable-handle>
 
-                            <resizable-panel id="topnav" :default-size="90">
-                                <div class="flex flex-col ">
-                                    <navigation-top :title="$route.name"></navigation-top>
+                <resizable-panel id="topnav" :default-size="90">
 
-                                    <!-- Page Content -->
-                                    <router-view :key="$route.fullPath"></router-view>
-                                    <!-- Page Content -->
-                                </div>
+                    <div class="flex flex-col">
+                        <navigation-top :title="$route.name"></navigation-top>
 
-                            </resizable-panel>
-                        </resizable-panel-group>
+                        <!-- Page Content -->
+                        <router-view :key="$route.fullPath"></router-view>
+                        <!-- Page Content -->
                     </div>
 
-                    <Toaster />
-                </div>
-            </div>
+                </resizable-panel>
+            </resizable-panel-group>
         </div>
+
+        <Toaster />
     </div>
 
     <!-- @include('includes.footer') -->
