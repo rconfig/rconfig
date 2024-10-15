c73066f10d1fcd0de2fb74f3a2090936c4ac44db
Stephen Stack <stephen@rconfig.com>
2024-10-15 15:53:41 +0100

    feat: add form components and integrate vee-validate for form handling

---
+++ resources/js/components/ui/form/FormControl.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Slot } from 'radix-vue';
+import { useFormField } from './useFormField';
+
+const { error, formItemId, formDescriptionId, formMessageId } = useFormField();
+</script>
+
+<template>
+  <Slot
+    :id="formItemId"
+    :aria-describedby="
+      !error ? `${formDescriptionId}` : `${formDescriptionId} ${formMessageId}`
+    "
+    :aria-invalid="!!error"
+  >
+    <slot />
+  </Slot>
+</template>
