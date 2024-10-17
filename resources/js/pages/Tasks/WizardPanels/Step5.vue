e3e2b78d298f84739a0d1d7894f18330b5f4a0be
Stephen Stack <stephen@rconfig.com>
2024-10-17 10:08:24 +0100

    feat: enhance Step5 component with detailed task and tag information display

--- resources/js/pages/Tasks/WizardPanels/Step5.vue
+++ resources/js/pages/Tasks/WizardPanels/Step5.vue
@@ -18,6 +18,67 @@ const handleCheck = value => {
   <div>
     STEP5
     <h3 class="mb-5 text-lg font-medium text-gray-900 dark:text-white">Finalize Task</h3>
-    <pre> {{ model }}</pre>
+    <!-- <pre> {{ model }}</pre> -->
+
+    <div class="container">
+      <div class="card">
+        <h2>Task Details</h2>
+        <p>
+          <strong>Task Name:</strong>
+          <span class="badge">{{ model.task_name }}</span>
+        </p>
+        <p>
+          <strong>Description:</strong>
+          {{ model.task_desc || 'No description provided' }}
+        </p>
+        <p>
+          <strong>Command:</strong>
+          {{ model.task_command }}
+        </p>
+        <p>
+          <strong>Categories:</strong>
+          {{ model.task_categories }}
+        </p>
+        <p>
+          <strong>Devices:</strong>
+          {{ model.task_devices }}
+        </p>
+        <p>
+          <strong>Tags:</strong>
+          {{ model.task_tags }}
+        </p>
+      </div>
+      <div class="card">
+        <h3>Tag Information</h3>
+
+        <div
+          class="ml-10 tag"
+          v-for="tag in model.tag">
+          <p>
+            <strong>Tag Name:</strong>
+            <span class="badge">{{ tag.tagname }}</span>
+          </p>
+          <p>
+            <strong>Description:</strong>
+            {{ tag.tagDescription || 'No description provided' }}
+          </p>
+          <p>
+            <strong>Created At:</strong>
+            {{ tag.created_at }}
+          </p>
+          <p>
+            <strong>Updated At:</strong>
+            {{ tag.updated_at }}
+          </p>
+          <p>
+            <a
+              href="{{ tag.view_url }}"
+              class="badge">
+              View Tag
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
