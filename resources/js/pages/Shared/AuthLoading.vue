a01bac962260d4c7abce745f61a092a041978925
Stephen Stack <stephen@rconfig.com>
2024-11-17 06:47:45 +0000

    Fix image source path in AuthLoading component for correct logo display

--- resources/js/pages/Shared/AuthLoading.vue
+++ resources/js/pages/Shared/AuthLoading.vue
@@ -8,7 +8,7 @@ defineProps({});
   <div class="flex items-center justify-center text-sm">
     <img
       class="w-8 h-8 animate-spin-slow"
-      src="images/new/white/hex_logo_white_original_32.png"
+      src="/images/new/white/hex_logo_white_original_32.png"
       alt="rConfig Logo" />
   </div>
 </template>
