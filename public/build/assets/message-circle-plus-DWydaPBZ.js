395b6bce1dab291b776cd6fde699d4cafc76fadb
danielrConfig <daniel@rconfig.com>
2025-12-15 12:11:38 +0000

    nrb push

--- public/build/assets/message-circle-plus-DWydaPBZ.js
+++
@@ -1,6 +0,0 @@
-import{h as e}from"./app-BdO4bxZX.js";/**
- * @license lucide-vue-next v0.525.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const a=e("message-circle-plus",[["path",{d:"M7.9 20A9 9 0 1 0 4 16.1L2 22Z",key:"vv11sd"}],["path",{d:"M8 12h8",key:"1wcyev"}],["path",{d:"M12 8v8",key:"napkw2"}]]);export{a as M};
